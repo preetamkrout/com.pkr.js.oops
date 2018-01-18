d86c14441b6e4e64123708657bffc179606d0ec7
Preetam Kajal Rout <preetam.krout@gmail.com>
2018-01-18 12:13:44 +0530

    prototype based inheritance

--- app.js
+++ app.js
@@ -13,21 +13,56 @@
 // ninja.dance();
 
 
-let newObj = { a: 1, b: 2, c: 3};
-let newObjKeys = Object.keys(newObj);
+// let newObj = { a: 1, b: 2, c: 3};
+// let newObjKeys = Object.keys(newObj);
 
-console.log(newObjKeys);
+// console.log(newObjKeys);
 
-let mutatedObj = { a: 1, b: 3, c: 4};
-Object.prototype.keys = function () {
-    let keys = [];
-    for (p in this) keys.push(p);
-    return keys;
+// let mutatedObj = { a: 1, b: 3, c: 4};
+// Object.prototype.keys = function () {
+//     let keys = [];
+//     for (p in this) keys.push(p);
+//     return keys;
+// }
+// console.log(mutatedObj.keys());
+// let newObjKeysAfter = [];
+// console.log(newObj.keys());
+// for (p in newObj) {
+//     newObjKeysAfter.push(p);
+// }
+// console.log(newObjKeysAfter);
+
+function Person (firstName, lastName) {
+    if (!(this instanceof arguments.callee)) {
+        return new Person(firstName, lastName);
+    }
+    this.firstName = firstName;
+    this.lastName = lastName;
 }
-console.log(mutatedObj.keys());
-let newObjKeysAfter = [];
-console.log(newObj.keys());
-for (p in newObj) {
-    newObjKeysAfter.push(p);
+
+Person.prototype.walk = function (steps) {
+    let noOfSteps = steps || 100;
+    console.log(`${this.firstName} ${this.lastName} haved walked for ${noOfSteps} today`);
 }
-console.log(newObjKeysAfter);
+
+function Ninja (firstName, lastName) {
+    if (!(this instanceof arguments.callee)) {
+        return new Ninja(firstName, lastName);
+    }
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+
+Ninja.prototype = new Person();
+
+Ninja.prototype.jump = function (buildings) {
+    let noOfBuildings = buildings || 100;
+    console.log(`${this.firstName} ${this.lastName} haved walked for ${noOfBuildings} today`);
+}
+
+const bruce = Person("Bruce", "Wayne"); //Wrong intialization. However, constructor should handle the error for being user-friendly
+bruce.walk(240);
+
+const batman = new Ninja("The", "Batman");
+batman.walk(255);
+batman.jump(3);
